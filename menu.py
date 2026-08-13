d411bfece87786b4ed9eaaf0f39e087fe0885816
Nathacha Martins Andarde Oliveira <nathachamartinsandrade@gmail.com>
2026-08-13 17:44:48 -0300

    feat(ap01-git): Implementa interface usuario

--- menu.py
+++ menu.py
@@ -1,9 +1,25 @@
 def menu():
     while True:
         print("\n1. Somar")
-
-         elif opcao == 1:
-            print("Você escolheu Somar")
-
         print("2. Subtrair")
+        print("3. Multiplicar")
+        print("4. Dividir")
+        print("0. Sair")
+
+        opcao = int(input("Escolha uma opção: "))
+
+        if opcao == 0:
+            print("Saindo...")
+            break
+
+        elif opcao == 1:
+            print("Você escolheu Somar")
+        elif opcao == 2:
+            print("Você escolheu Subtrair")
+        elif opcao == 3:
+            print("Você escolheu Multiplicar")
+        elif opcao == 4:
+            print("Você escolheu Dividir")
+        else:
+            print("Opção inválida!")
 menu()
